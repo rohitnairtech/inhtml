7acbcafc84939c451e9c1b55357de456b3a36811
rohitnairtech <puthenrohit@gmail.com>
2020-09-18 17:00:24 +0530

    struct changed

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 "use strict";
 const {readFileSync} = require('fs');
 
-const inHtml = {
-    html : '',
-    load: function(fName){
-        this.html = readFileSync(fName, 'utf-8').replace(/\s\s+/g, ' ');
-    },
-    renderLoad: function(data){
-        let {html} = this;
-        for(let changeable in data){
-            html = html.replace(new RegExp('#{'+changeable+'}','g'),data[changeable]);
-        }
-        return html;
-    },
-    render: (fName, data)=>{
-        let html = readFileSync(fName, 'utf-8').replace(/\s\s+/g, ' ');
-        for(let changeable in data){
-            html = html.replace(new RegExp('#{'+changeable+'}','g'), data[changeable]);
-        }
-        return html;
-    }
+const inHtml = (filename) => {
+
+    const file = readFileSync(filename, 'utf-8').replace(/\s\s+/g, ' ');
+    
+    return {
+            render: data => {
+                let html = file;
+                for(let changeable in data){
+                    html = html.replace(new RegExp('#{'+changeable+'}','g'),data[changeable]);
+                }
+                return html;
+            }
+        };
 }
 
 module.exports = inHtml;
-
-// inHtml.load('./mail/mailer.html');
-// const rep = inHtml.renderLoad({link:'somshit.in', nameBtn: 'SendWa'});
-// console.log(rep);
-// console.log(inHtml.render('./mail/mailer.html', {link:'somshit.insss'}));

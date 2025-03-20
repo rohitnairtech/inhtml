66f4831b20ad1abc4c20f2782ae7d906fcdc8fed
Rohit Nair <puthenrohit@gmail.com>
2025-03-20 14:54:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 "use strict";
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 
-module.exports = (filename) => {
+module.exports = (filename, options = {}) => {
+  const file = readFileSync(filename, 'utf-8').replace(/\s{2,}/g, ' ');
 
-    const file = readFileSync(filename, 'utf-8').replace(/\s\s+/g, ' ');
-    
-    return {
-            render: data => {
-                let html = file;
-                for(let changeable in data){
-                    html = html.replace(new RegExp('#{'+changeable+'}','g'),data[changeable]);
-                }
-                return html;
-            }
-        };
-}
+  const { escapeHtml = false, partials = {} } = options;
+
+  const escape = (str) => str.replace(/[&<>'"`]/g, (char) => {
+    const escapeMap = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#39;',
+      '`': '&#96;'
+    };
+    return escapeMap[char] || char;
+  });
+
+  const renderPartials = (html) => {
+    return html.replace(/#{>\s*(\w+)\s*}/g, (_, key) => {
+      if (partials[key]) {
+        return renderPartials(partials[key]);
+      }
+      return `#{>${key}}`;
+    });
+  };
+
+  return {
+    render: (data) => {
+      let html = renderPartials(file);
+      
+      return html.replace(/#{(\w+)}/g, (_, key) => {
+        const value = data[key];
+        if (value === undefined) return `#{${key}}`;
+        return escapeHtml ? escape(String(value)) : String(value);
+      });
+    },
+  };
+};
